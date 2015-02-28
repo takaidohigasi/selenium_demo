7b9a886087fbd9fd52578cbab291eaee86a78b7f
takaidohigasi <takaidohigasi@gmail.com>
2015-03-01 02:07:40 +0900

    add code insertion

--- bin/ruby_warrior_demo.rb
+++ bin/ruby_warrior_demo.rb
@@ -1,4 +1,9 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
 require 'selenium-webdriver'
+# @see http://www.seleniumhq.org/
+# @see https://code.google.com/p/selenium/wiki/RubyBindings
 
 # initialize browser
 driver = Selenium::WebDriver.for(:firefox)
@@ -14,6 +19,20 @@ driver.find_element(:class, 'button').click
 driver.find_element(:class, 'name').find_element(:tag_name, 'input').clear
 driver.find_element(:class, 'name').find_element(:tag_name, 'input').send_keys('taka-h')
 driver.find_element(:id, 'create-warrior').click
+
+# wait for a specific element to show up
+sleep 5
+
+wait = Selenium::WebDriver::Wait.new(timeout: 5)
+wait.until { driver.find_element(class: 'editor') }
+
+# clear text
+driver.find_element(class: 'ace_text-input').clear
+
+# input answer
+code = File.read("answer_code/level01.txt", :encoding => Encoding::UTF_8).gsub(/\n/, ';')
+driver.find_element(class: 'ace_text-input').send_keys(code)
+sleep 20
 
 # quit
 driver.quit
